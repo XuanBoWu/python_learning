8fced0bb0add964324d125b3e3833df75d663406
XuanBoWu <wuxuanbo@foxmail.com>
2024-01-13 04:12:28 +0800

    练习8-9 ～ 练习8-11

---
+++ Chapter_8/messages_8-9.py
@@ -0,0 +1,10 @@
+def show_messages(messages):
+    for message in messages:
+        print(message)
+
+messages = [
+    "create mode 100644 Chapter_8/album_8-7.py",
+    "create mode 100644 Chapter_8/city_name_8-6.py",
+    "create mode 100644 Chapter_8/user_album_8-8.py"
+    ]
+show_messages(messages)
---
+++ Chapter_8/send_messages_8-10.py
@@ -0,0 +1,23 @@
+def show_messages(messages):
+    for message in messages:
+        print(message)
+
+def send_messages(original_messages, sent_messages):
+    while original_messages:
+        message = original_messages.pop()
+        sent_messages.append(message)
+
+original_messages = [
+    "create mode 100644 Chapter_8/album_8-7.py",
+    "create mode 100644 Chapter_8/city_name_8-6.py",
+    "create mode 100644 Chapter_8/user_album_8-8.py"
+    ]
+
+sent_messages = []
+send_messages(original_messages, sent_messages)
+
+print("original_messages:")
+show_messages(original_messages)
+
+print("\nsent_messages:")
+show_messages(sent_messages)
