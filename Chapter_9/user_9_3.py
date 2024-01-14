165d8038616baedda93f784e204004df7de83fad
XuanBoWu <wuxuanbo@foxmail.com>
2024-01-15 01:43:32 +0800

    修改练习9-3

--- Chapter_9/user_9_3.py
+++ Chapter_9/user_9_3.py
@@ -1,11 +1,11 @@
 class User:
-    name = ''
-    def __init__(self, first_name, last_name, gender, age) -> None:
+    def __init__(self, first_name, last_name, gender, age, name='') -> None:
         self.first_name = first_name
         self.last_name = last_name
         self.name = self.first_name + " " + self.last_name
         self.gender = gender
         self.age = age
+
 
     def describe_user(self):
          
