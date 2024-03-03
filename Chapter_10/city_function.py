47378a87fe565a3f35566f5904e8eac6bfbb5fc0
XuanBoWu <wuxuanbo@foxmail.com>
2024-03-04 05:50:46 +0800

    Completed practice 11-2

--- Chapter_10/city_function.py
+++ Chapter_10/city_function.py
@@ -1,3 +1,6 @@
-def formatted_city_country_name(city, country):
+def formatted_city_country(city, country, population=""):
     format_city_name = f"{city}, {country}"
-    return format_city_name.title()
+    format_population = ""
+    if population != "":
+        format_population = f" - population {population}"
+    return format_city_name.title() + format_population
--- Chapter_10/test_city_country.py
+++ Chapter_10/test_city_country.py
@@ -1,10 +1,13 @@
 import unittest
-from city_function import formatted_city_country_name
+from city_function import formatted_city_country
 
 class CityTestCase(unittest.TestCase):
     def test_city_name(self):
-        formated_city_name = formatted_city_country_name("shenzhen", "china")
+        formated_city_name = formatted_city_country("shenzhen", "china")
         self.assertEqual(formated_city_name, "Shenzhen, China")
 
+    def test_city_population(self):
+        formatted_name = formatted_city_country("shenzhen", "china", 20000000)
+        self.assertEqual(formatted_name, "Shenzhen, China - population 20000000")
 if __name__ == '__main__':
     unittest.main()
