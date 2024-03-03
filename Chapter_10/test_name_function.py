a48e5658bf61d212f8ca94f6b0c4c31942559f8c
XuanBoWu <wuxuanbo@foxmail.com>
2024-03-04 05:18:17 +0800

    包含中间名情况的test case

--- Chapter_10/test_name_function.py
+++ Chapter_10/test_name_function.py
@@ -5,10 +5,13 @@ from name_function import get_formatted_name
 class NameTestCase(unittest.TestCase):
     """Test 'name_function.py'"""
 
-    def test_first_last_name(self):
-        
+    def test_first_last_name(self):        
         formatted_name = get_formatted_name('alex', 'wu')
         self.assertEqual(formatted_name, 'Alex Wu')
 
+    def test_first_last_middle_name(self):
+        "测试 中间名是否可以正确处理"
+        formatted_name = get_formatted_name("alex", "wu", "max")
+        self.assertEqual(formatted_name, "Alex Max Wu")
 if __name__ == '__main__':
     unittest.main()
