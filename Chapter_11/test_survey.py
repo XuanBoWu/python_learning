66f0d4bfe3a377c9c461f6a06348dcea1684e8f1
XuanBoWu <wuxuanbo@foxmail.com>
2024-03-05 01:36:21 +0800

    添加 setUp方法 优化测试资源分配，提高重用性

--- Chapter_11/test_survey.py
+++ Chapter_11/test_survey.py
@@ -3,25 +3,26 @@ from survey import AnonymousSurvey
 
 class TestAnonymousSurvey(unittest.TestCase):
     """ 针对AnonymousSurvey类的测试 """
+    def setUp(self):
+        """
+        创建一个调查对象和一组回复，供测试方法使用
+        """
+        question = "What language did you first learn to speak?"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English', 'Chinese', 'Japnese']
+
     def test_store_single_response(self):
         """ 测试单个回复会被妥善存储 """
-
-        question = "What language did you first learn to speak?"
-        my_survey = AnonymousSurvey(question)
-
-        my_survey.store_response('English')
-        self.assertIn('English', my_survey.responses)
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
     
     def test_store_three_response(self):
         """ 测试三个回复会被妥善存储 """
-        question = "What language did you first learn to speak?"
-        my_survey = AnonymousSurvey(question)
-        responses = ['English', 'Chinese', 'Japnese']
-        for respones in responses:
-            my_survey.store_response(respones)
+        for respones in self.responses:
+            self.my_survey.store_response(respones)
         
-        for response in responses:
-            self.assertIn(response, my_survey.responses)
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
         
         
 
