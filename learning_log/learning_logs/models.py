44c47342ce9bc6b5a694b9238c3ea3592d0b0ff4
XuanBoWu <wuxuanbo@foxmail.com>
2024-03-14 22:16:31 +0800

    完成 Entry 模版

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -12,3 +12,14 @@ class Topic(models.Model):
         """ Return the string representation of the model """
         return self.text
 
+class Entry(models.Model):
+    """ specific knowledge acquired about a particular topic. """
+    topic = models.ForeignKey(Topic, on_delete = models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self) -> str:
+        return f"{self.text[:50]}..."
